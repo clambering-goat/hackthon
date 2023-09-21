436e41e91b01c9adeab00dc6b5cd2a016c82a178
Trien Do <trien@MAC-LON-011.broadband>
2023-09-21 13:31:25 +0100

    Wrong way to compare but ok

--- read_input.py
+++ read_input.py
@@ -1,18 +1,61 @@
 import csv
-print('hello')
-file_path = "../catlog.csv"
-out_array = []
-with open(file_path, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        out_array.append(out_array)
-        # print(', '.join(row))
 
-print(out_array)
+def input_to_2d_array(file_path):
+    out_array = []
+    with open(file_path) as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            out_array.append(row)
+            # print(', '.join(row))
+    return out_array
 
-for count_1, q in enumerate(out_array):
-    for count_2, w in enumerate(q):
-        if w is None:
-            print("hi")
 
-print("done")
+def compare_dic_cat(dictionary, catlog):
+    cat_dict = {}
+    tables = {}
+    for index in range(1, len(catlog)):
+        row = catlog[index]
+        fields = set()
+        if row[0] in cat_dict:
+            tables = cat_dict[row[0]]
+        else:
+            cat_dict[row[0]] = {}
+
+        if row[2] in tables:
+            fields = tables[row[2]]
+            fields.add(row[3])
+        else:
+            tables[row[2]] = {row[3]}
+    #print(cat_dict)
+
+    for index in range(1, len(dictionary)):
+        row = dictionary[index]
+        if row[0] in cat_dict:
+            tables = cat_dict[row[0]]
+            if row[2] in tables:
+                fields = tables[row[2]]
+                if not row[3] in fields:
+                    print('Missing Field: ' + row[3])
+            else:
+                print('Missing Entity: ' + row[2])
+        else:
+            print('Missing DB: ' + row[0])
+
+
+    # dictionary_dic={}
+    # for count, q in enumerate(dictionary[1:]):
+    #     key_dic = (', '.join([ q[0], q[2], q[3]]))
+    #     dictionary_dic[key_dic]=q
+    #     print(key_dic)
+    #
+    # print("----------------")
+    # for count, q in enumerate(catlog[1:]):
+    #     key_dic = (', '.join([, q[0], q[2], q[3]]))
+    #     print(key_dic)
+    return ''
+
+print("hello")
+catlog = input_to_2d_array("./catlog.csv")
+dictionary = input_to_2d_array("./data_dic.csv")
+dictionary2 = input_to_2d_array("./data_dic_2.csv")
+compare_dic_cat(dictionary, catlog)
