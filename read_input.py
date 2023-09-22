b1bb744cc532ca77f690dd28713c57023e6a36ea
Trien Do <trien@MAC-LON-011.broadband>
2023-09-22 04:42:42 +0100

    Add missing to results and clear rs every time

--- read_input.py
+++ read_input.py
@@ -1,10 +1,6 @@
 import csv
-import matplotlib.pyplot as plt
-import numpy as np
-from gen_png_chart import *
 output = []
-
-
+missing = []
 def input_to_2d_array(file_path):
     out_array = []
     with open(file_path) as csvfile:
@@ -12,8 +8,6 @@ def input_to_2d_array(file_path):
         for row in spamreader:
             out_array.append(row)
     return out_array
-
-
 def create_dic_for_input(data):
     data_dict = {}
 
@@ -38,20 +32,26 @@ def create_dic_for_input(data):
 
     return data_dict
 
-
-def calculate_field_level(dict, cat, name):
-    print(name)
+def calculate_field_level(dict, cat, entity, db_name):
+    print(entity)
     total = 0
     found = 0
     for field in cat:
         total = total + 1
         if field in dict:
             found = found + 1
+        else:
+            print("Missing attribute " + field)
+            missing.append("Database " + db_name + " ==> entity " +
+                           entity + " missing attribute " + field)
     rs = found*100/total
     print("Completeness of the table: " + str(rs) + " %\n")
-    output.append({'label': name, 'value': rs})
+    output.append({'label': entity, 'value': rs})
+
 
 def calculate_completeness(dictionary, catalog):
+    output.clear()
+    missing.clear()
     # Get db name
     my_db_name = list(dictionary.keys())[0]
     # Get catalog for dict
@@ -63,16 +63,19 @@ def calculate_completeness(dictionary, catalog):
         if table in dictionary[my_db_name]:
             print("Found " + table)
             calculate_field_level(
-                dictionary[my_db_name][table], my_cat[table], table)
+                dictionary[my_db_name][table], my_cat[table], table, my_db_name)
             found = found + 1
         else:
+            output.append({'label': table, 'value': 0})
+            missing.append("Database " + my_db_name +
+                           " missing table " + table)
             print("Not found " + table)
     rs = found * 100 / total
     print("\nCompleteness of the db: " + str(rs) + " %")
     output.append({'label': my_db_name, 'value': rs})
     print(output)
     # gen_chart(["Done", "Not done"], [rs, 100-rs], my_db_name + ".png")
-    return output
+    return {"stats": output, "missing": missing}
 
 
 def main():
@@ -87,11 +90,12 @@ def main():
         dictionary_dict = create_dic_for_input(dictionary_arr)
         calculate_completeness(dictionary_dict, catalog_dict)
 
+
 def compare():
     catalog_arr = input_to_2d_array("./uploads/csv/catalog.csv")
     catalog_dict = create_dic_for_input(catalog_arr)
 
     dictionary_arr = input_to_2d_array("./uploads/csv/dictionary.csv")
     dictionary_dict = create_dic_for_input(dictionary_arr)
-        
+
     return calculate_completeness(dictionary_dict, catalog_dict)
